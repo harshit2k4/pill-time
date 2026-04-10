299cb973e1578a125ee99a69385f77cc92b727b0
Harshit <84241825+harshit2k4@users.noreply.github.com>
2026-04-10 10:24:08 +0530

    chore(strings): update app strings

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -2,4 +2,8 @@ class AppStrings {
   static const String appName = "Medicine Reminder";
   static const String boxMedicines = "medicines";
   static const String boxLogs = "dose_logs";
+
+  static const String safetyWarningTitle = "Safety Warning";
+  static const String safetyWarningBody =
+      "You are marking multiple medicines as taken very quickly. Taking too many medicines at once can be harmful. Please consult your doctor if you missed multiple doses.";
 }
